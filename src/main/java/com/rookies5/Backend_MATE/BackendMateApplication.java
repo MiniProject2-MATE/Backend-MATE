b368bac13c59e401748b64af9cc05f0faf922926
Jangdochi <hunjoon0501@naver.com>
2026-03-30 13:05:12 +0900

    first commit

---
+++ src/main/java/com/rookies5/Backend_MATE/BackendMateApplication.java
@@ -0,0 +1,13 @@
+package com.rookies5.Backend_MATE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rookies5/Backend_MATE/BackendMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rookies5.Backend_MATE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
